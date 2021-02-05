e30bdc908d91328ec2920c86c06e7a789b646280
Cristian Mejia <crmejiam@unal.edu.co>
2021-02-05 13:45:08 -0500

    Realize all homework steps reaching the final error

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-echo "Hello from backend!"
+
+# Go to shared folder where the binary file is ubicated
+cd /shared
+
+# Define Environment variables
+sudo echo 'export PORT=4001' >> /etc/profile
+
+# Execute the binary file on background
+cd /shared/
+nohup ./vuego-demoapp > server.out 2>&1 &
+
+# To verify correct execution of the process
+ps -aux
+echo $PORT
